16ee3b90c9e999d9c6f8df4b7607fead0016f453
RubenRR01 <93340057+RubenRR01@users.noreply.github.com>
2022-03-18 07:20:33 +0100

    Delete start-assets directory

--- start-assets/Assets/Actors/actor.gd
+++
@@ -1,9 +0,0 @@
-extends KinematicBody2D
-class_name Actor
-export var speed := Vector2(300.0, 300.0)
-var velocity := Vector2.ZERO
-export var gravity := 3000.0
-func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta
-	velocity.y = min(velocity.y, speed.y)
-	velocity = move_and_slide(velocity)
